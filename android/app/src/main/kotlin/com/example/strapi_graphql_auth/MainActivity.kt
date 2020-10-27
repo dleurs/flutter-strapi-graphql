9cbf882a6c9a41d09af5f9be37f1194387bde68a
Dimitri Leurs <dimitri.leurs@numberly.com>
2020-10-27 11:14:37 +0100

    Backend and frontend repo

--- android/app/src/main/kotlin/com/example/strapi_graphql_auth/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.strapi_graphql_auth
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ frontend/android/app/src/main/kotlin/com/example/flutter_strapi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_strapi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
