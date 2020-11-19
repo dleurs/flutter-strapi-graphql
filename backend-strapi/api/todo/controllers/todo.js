de8f3d9fdeae4009c93d14a68439e152e0c53452
Dimitri Leurs <dimitri.leurs@numberly.com>
2020-11-19 14:53:23 +0100

    Backend strapi code put inside /user

--- backend-strapi/api/todo/controllers/todo.js
+++ backend-strapi/api/todo/controllers/todo.js
@@ -5,21 +5,22 @@ module.exports = {
    * Retrieve a record.
    *
    * @return {Object}
-   */
+   
 
-  async findOneByUsername(_, { username }) {
-    //console.log(ctx.params.username);
+  async findOneByUsername(ctx, { username }) {
     //var username = ctx.params.username;
 
 
     console.log(username);
+    console.log(ctx.params.username);
     //const { username } = ctx.params;
     //console.log(username);
+    console.log(strapi.plugins['users-permissions'].services.user);
 
-    const entity = await strapi.plugins['users-permissions'].services.user.fetch({ username });
+    const entity = await strapi.plugins['users-permissions'].services.user.find({ username: "dimi_du_24@hotmail.fr" });
     console.log(entity);
     //const entity = await strapi.services.user.find({ "username": ctx.params.username });
     //return sanitizeEntity(entity, { model: strapi.models.user });
-    return sanitizeEntity(entity, { model: strapi.plugins['users-permissions'].models.user });
-  },
+    return sanitizeEntity(entity.email, { model: strapi.plugins['users-permissions'].models.user });
+  }, */
 };
