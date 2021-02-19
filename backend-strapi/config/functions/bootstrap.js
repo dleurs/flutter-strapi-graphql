85a77a36b1b399f4f0e9352ad47cd1f08825c338
Dimitri Leurs <dimitri.leurs@numberly.com>
2021-02-19 17:08:47 +0100

    Trying to setup socket io

--- backend-strapi/config/functions/bootstrap.js
+++ backend-strapi/config/functions/bootstrap.js
@@ -10,4 +10,21 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = () => {
+    var io = require('socket.io')(strapi.server, {
+        cors: {
+          origin: "http://localhost:3000",
+          methods: ["GET", "POST"],
+          allowedHeaders: ["my-custom-header"],
+          credentials: true
+        }
+    });
+
+    io.on('connection', function(socket) {
+          socket.on('join', ({ username, room }) => {
+              console.log("user connected");
+              console.log("username is ", username);
+              console.log("room is...", room)
+          })
+      });
+  };
